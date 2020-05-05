cfe51d3b8fbd1e3744d99edfd149d410cf93c632
Jess <jess.myee@gmail.com>
2020-05-04 22:18:47 -0400

    Allow users to log out

--- users/urls.py
+++ users/urls.py
@@ -9,4 +9,6 @@ app_name = 'users'
 urlpatterns = [
     #Login page
     url(r'^login/', login, {'template_name': 'users/login.html'}, name = 'login'),
+    #Logout page
+    url(r'^logout/', views.logout_view, name='logout'),
 ]
