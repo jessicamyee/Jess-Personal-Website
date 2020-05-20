0433a2bccc85be5e9da969fc949a3fcb0ad378d3
Jess <jess.myee@gmail.com>
2020-05-19 23:30:05 -0400

    Restricting access throughout the website to only those who have an account

--- app_logs/views.py
+++ app_logs/views.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
 
 
@@ -16,12 +17,14 @@ def index(request):
     """The home page for Learning Log"""
     return render(request, 'app_logs/index.html')
 
+@login_required
 def topics(request):
     """Show all topics."""
     topics = Topic.objects.order_by('date_added')
     context = {'topics': topics}
     return render(request, 'app_logs/topics.html', context)
 
+@login_required
 def topic(request, topic_id):
     """Show a single topic and all its entries."""
     topic = Topic.objects.get(id=topic_id)
@@ -29,6 +32,7 @@ def topic(request, topic_id):
     context = {'topic': topic, 'entries': entries} 
     return render(request, 'app_logs/topic.html', context)
 
+@login_required
 def new_topic(request):
     """Add a new topic."""
     if request.method != 'POST':
@@ -45,6 +49,7 @@ def new_topic(request):
     return render(request, 'app_logs/new_topic.html', context)
 
 
+@login_required
 def new_entry(request, topic_id):
     """Add a new entry for a particular topic."""
     topic = Topic.objects.get(id=topic_id)
@@ -66,6 +71,7 @@ def new_entry(request, topic_id):
 
 
 
+@login_required
 def edit_entry(request, entry_id):
     """Edit an existing entry."""
     entry = Entry.objects.get(id=entry_id)
